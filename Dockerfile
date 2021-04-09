e4ee271468b33c578d11d6b9753298fca3a0b246
Bilal ATLI <bilal@socialtradex.com>
2021-04-10 00:22:45 +0300

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,9 @@
-
-FROM node:12
-
-RUN npm install pm2 -g
-ENV PM2_PUBLIC_KEY dz5rlwgbn3w9eo7
-ENV PM2_SECRET_KEY 9z3144tyxvf6mlk
+FROM node:14.15.4-alpine as builder
 
 WORKDIR /usr/src/app
 
 COPY package*.json ./
-
-RUN npm install
-
+RUN npm install --silent
 COPY . .
 
-CMD ["pm2-runtime", "index.js", "--restart-delay=3000"]
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -5,81 +5,113 @@ const WebSocket = require("ws");
 const app = express();
 
 const axios = require("axios");
-const { response } = require("express");
+const {response} = require("express");
+let ws = null;
 
-//TYPE 3 - SOCKET
-
-//Default
-app.get("*", (req, res) => {
-  res.status(201).json("Hello From FIG Notifier - v0.0.0.1");
-});
-
-//Start
+const RECONNECT_DELAY = 5;
 const DEFAULT_PORT = Number(process.env.PORT || 5110);
-const NOTIFY_URL = process.env.NOTIFY_URL;
+const NOTIFY_URL = process.env.NOTIFY_URL || null;
+const SOCKET_URL = process.env.SOCKET_URL || null;
 
-app.listen(DEFAULT_PORT, () => {
-  console.log(`listening on ${DEFAULT_PORT}`);
+app.get("*", (req, res) => {
+    res.status(201).json("Hello From FIG Notifier - v0.0.0.1");
 });
 
-const ws = new WebSocket(`${process.env.SOCKET_URL}`, {
-  perMessageDeflate: false,
+const httpServer = app.listen(DEFAULT_PORT, () => {
+    console.log(`listening on ${DEFAULT_PORT}`);
 });
 
-ws.on("open", (data) => {
-  console.log("CONNECT");
-  console.log(data, "DATA OPENN");
-});
+function initializeSocketConnection() {
+    if (SOCKET_URL === null) {
+        httpServer.close();
+        throw new Error(`Socket connection url is not defined. Closing application`);
+    }
+    ws = new WebSocket(`${process.env.SOCKET_URL}`, {
+        perMessageDeflate: false
+    });
 
-ws.on("message", (msg) => {
-  console.log("New Transaction");
-  msg = JSON.parse(msg);
+    setEventListeners();
+    heartBeat();
+}
 
-  if (msg.type === 2) {
-    const data = JSON.parse(msg.data);
-    msgChecker(data[0]);
-  }
-});
+function setEventListeners() {
+    if (ws === null || ws.isConnected === false) {
+        console.warn(`Socket is not connected or initialized`);
+    }
+    ws.on("open", (data) => {
+        console.log(`SOCKET: Connection established`);
+        if (data !== null && data !== '' && typeof data !== 'undefined') {
+            console.log(`Welcome Data : ${data}`);
+        }
+    });
+    ws.on("message", (msg) => {
+        msg = JSON.parse(msg);
+        if (typeof msg.type !== 'undefined' && typeof msg.data !== 'undefined' && msg.type === 2) {
+            const data = JSON.parse(msg.data);
+            const blockNumber = data[0].index;
+            console.log(`-------------------------------------------------`);
+            console.log(`Reading ${blockNumber} block for new transactions`);
+            msgChecker(data[0]);
+        }
+    });
+    ws.on("close", () => {
+        onSocketError();
+    });
+    ws.on("error", (error) => {
+        console.log(`Socket error: ${error}`);
+        onSocketError();
+    });
+}
+
+function heartBeat() {
+    if (ws === null || ws.readyState !== 1) {
+        return;
+    }
+    // console.log(`  # Socket heartbeat was sent`);
+    ws.send(`heartbeat`);
+    setTimeout(heartBeat, 500);
+}
+
+function onSocketError() {
+    console.warn(`Socket connection is disconnected. Trying to reconnect after ${RECONNECT_DELAY} second...`);
+    ws.close();
+    ws = null;
+    setTimeout(function () {
+        initializeSocketConnection();
+    }, RECONNECT_DELAY * 1000);
+}
 
 const msgChecker = async (transaction) => {
-  const { hash } = transaction;
-  let nonce = Math.floor(Math.random() * 1000000);
+    const {hash, index} = transaction;
+    let nonce = Math.floor(Math.random() * 1000000);
 
-  if (transaction.data.length < 2) {
-    console.log("There is no transaction in the block.");
-
-    const response = await axios.post(`${NOTIFY_URL}`, {
-      txId: null,
-      receiver: null,
-      sender: null,
-      hash,
-      amount: 1,
-      nonce,
-    });
-
-    if (response.data.status === true) {
-      console.log(response.data.message);
+    if (transaction.data.length < 2) {
+        console.log(`${index} Transaction not found in current block`);
+        notify(index, {txId: null, receiver: null, sender: null, hash: hash, amount: 1, nonce: nonce});
+    } else {
+        const {receiver, sender, id, amount} = transaction.data[1];
+        notify(index, {txId: id, receiver: receiver, sender: sender, hash: hash, amount: amount, nonce: nonce});
     }
-    if (response.data.status === false) {
-      console.log(response.data.message);
-    }
-  } else {
-    const { receiver, sender, id, amount } = transaction.data[1];
-
-    const response = await axios.post(`${NOTIFY_URL}`, {
-      txId: id,
-      receiver,
-      sender,
-      hash,
-      amount,
-      nonce,
-    });
-
-    if (response.data.status === true) {
-      console.log(response.data.message);
-    }
-    if (response.data.status === false) {
-      console.log(response.data.message);
-    }
-  }
 };
+
+async function notify(blockNumber, transaction) {
+    // Return if notify url is not defined
+    if (NOTIFY_URL === null) {
+        return;
+    }
+    await axios.post(NOTIFY_URL, transaction)
+        .then(response => {
+            if (typeof response.data.status !== 'undefined') {
+                if (!!response.data.status === true) {
+                    console.log(`Notification completed. Answer from server : ${response.data.status}`);
+                } else {
+                    console.warn(`Notification is not completed : ${response.data.message || ''}`);
+                }
+            }
+        })
+        .catch(reason => {
+            console.warn(`  > Notification failed. Reason is ${reason}`);
+        });
+}
+
+initializeSocketConnection();
